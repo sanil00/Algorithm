9a1cd0f33526bd28742f4ceb842544fd1301b608
reyle3 <reyel3@naver.com>
2022-08-23 17:46:01 +0900

    dfs

--- 깊이우선탐색/여행경로/tmp.js
+++ 깊이우선탐색/여행경로/tmp.js
@@ -1,11 +1,37 @@
-function solution(n) {
-
-    let answer = 0
+function solution(tickets) {
+    let startIndex = ""
+    const visited = []
+    tickets.sort()
+    console.log(tickets)
+    tickets.find((value, index) => {
+        startIndex = index
+        return value[0] === "ICN"
+    })
+    const queue = [tickets[startIndex][1], visited]
+    visited[startIndex] = true
+    var answer = [tickets[startIndex][0], tickets[startIndex][1]]
+    while (queue.length) {
+        const destination = queue.shift()
+        tickets.map(([currentDest, nextDest], index) => {
+            if (currentDest === destination && !visited[index]) {
+                console.log(nextDest)
+                queue.push(nextDest)
+                answer.push(nextDest)
+                visited[index] = true
+            }
+        })
+    }
 
     return answer
 }
 
-let a = 5
+let a = [
+    ["ATL", "ICN"],
+    ["ATL", "SFO"],
+    ["ICN", "ATL"],
+    ["ICN", "SFO"],
+    ["SFO", "ATL"],
+]
 let b = [2, 4]
 let c = [2, 3, 4]
 document.querySelector("#answer").innerHTML = solution(a)
